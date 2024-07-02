88a295fe9e455c600a182663117b47068c5226fe
Nastya Belova <nastjabelova0@gmail.com>
2024-07-02 21:50:41 +0300

    implement classes Animal, Dog and Bird

--- src/main/java/com/epam/mjc/stage0/Animal.java
+++ src/main/java/com/epam/mjc/stage0/Animal.java
@@ -1,5 +1,21 @@
 package com.epam.mjc.stage0;
 
 public class Animal {
-
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+    public Animal (String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+    public String getDescription(){
+        return String.format("This animal is mostly %s. It has %s %s and %s fur.", color, numberOfPaws, changeWordPaws(), presenceOfFur());
+    }
+    private String presenceOfFur(){
+        return hasFur? "a" : "no";
+    }
+    private String changeWordPaws(){
+        return numberOfPaws == 1 ? "paw" : "paws";
+    }
 }
